3a15fa6db2f1ff33c44abaace7c18ecb746d1a98
RubenGomez1990 <rubengomez@paucasesnovescifp.cat>
2026-02-02 14:59:39 +0100

    Modificaciones para primer GET a Jikan

--- lib/providers/anime_providers.dart
+++ lib/providers/anime_providers.dart
@@ -11,15 +11,21 @@ class AnimeProvider extends ChangeNotifier {
   getOnDisplayAnime() async {
     print('getOnDisplayAnime');
     var url =
-        Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': '{http}'});
-
+        // Buscamos los animes top de temporada
+        Uri.https('api.jikan.moe', '/v4/top/anime');
     // Await the http get response, then decode the json-formatted response.
     var response = await http.get(url);
     if (response.statusCode == 200) {
       var jsonResponse =
           convert.jsonDecode(response.body) as Map<String, dynamic>;
-      var itemCount = jsonResponse['totalItems'];
-      print('Number of books about http: $itemCount.');
-    } else {}
+      final List<dynamic> listaAnimes = jsonResponse['data'];
+
+      var itemCount = listaAnimes.length;
+      print('Número de animes obtenidos: $itemCount.');
+      ;
+      print('Nombre del primer anime: ${listaAnimes[0]['title']}');
+    } else {
+      print('Error en la petición: ${response.statusCode}');
+    }
   }
 }
