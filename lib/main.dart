989cbd82be988a3d04ec9b590436cda750eb8e82
RubenGomez1990 <rubengomez@paucasesnovescifp.cat>
2026-02-02 12:25:01 +0100

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/screens/screens.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pel·lícules',
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext context) => const HomeScreen(),
+        'details': (BuildContext context) => const DetailsScreen(),
+      },
+      theme: ThemeData.light()
+          .copyWith(appBarTheme: const AppBarTheme(backgroundColor: Colors.indigo)),
+    );
+  }
+}
